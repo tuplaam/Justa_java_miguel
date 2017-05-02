8254acbada51adc842ffbe4c300fbafe0892c0d2
CAPACITACION-10 <vivelab@ufpso.edu.co>
2017-05-02 10:36:29 -0500

    Commit

--- app/src/main/java/com/android/curso/justa_java_miguel/Actividad_2.java
+++ app/src/main/java/com/android/curso/justa_java_miguel/Actividad_2.java
@@ -1,13 +1,69 @@
 package com.android.curso.justa_java_miguel;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class Actividad_2 extends AppCompatActivity {
+
+
+    private Button btnCambiarImagen;
+    private ImageView ivImagenActividad2;
+    private RadioButton rbAndroid;
+    private RadioButton rbIos;
+    private TextView tvRestablecer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_2);
+
+        btnCambiarImagen = (Button) findViewById(R.id.btnCambiarImagen);
+        ivImagenActividad2 = (ImageView) findViewById(R.id.ivImagenActividad2);
+        rbAndroid = (RadioButton) findViewById(R.id.rbAndroid);
+        rbIos = (RadioButton) findViewById(R.id.rbIos);
+        tvRestablecer = (TextView) findViewById(R.id.tvRestablecer);
+
     }
+
+
+
+
+
+    public void cambiarImagen (View v){
+
+
+        if(rbAndroid.isChecked()){
+           // Toast.makeText(this, "android", Toast.LENGTH_SHORT).show();
+            ivImagenActividad2.setImageResource(R.drawable.android);
+
+
+        } else if(rbIos.isChecked()){
+         //   Toast.makeText(this, "ios", Toast.LENGTH_SHORT).show();
+            ivImagenActividad2.setImageResource(R.drawable.apple);
+
+        }
+
+
+
+    }
+
+
+
+    public void restablecerImagen (View v){
+
+        if (tvRestablecer.isClickable()){
+            ivImagenActividad2.setImageResource(R.drawable.cara);
+
+        }
+
+
+
+    }
+    
 }
--- app/src/main/java/com/android/curso/justa_java_miguel/MainActivity.java
+++ app/src/main/java/com/android/curso/justa_java_miguel/MainActivity.java
@@ -1,13 +1,34 @@
 package com.android.curso.justa_java_miguel;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLoadActivity2; //variables globales
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //cargue los elementos que tiene este layout
+
+        btnLoadActivity2 = (Button) findViewById(R.id.btnLoadActivity2);
+
+
+
     }
+
+    public void loadActivity(View v){ //void por ser un metodo sin retorno
+
+        Intent load = new Intent(this, Actividad_2.class);
+
+        startActivity(load);
+
+    }
+
+
+
 }
